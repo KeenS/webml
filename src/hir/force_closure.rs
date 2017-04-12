2a16dc5fab426b761f391a24c6b4c8b72dc97b8f
Sunrin SHIMURA (keen) <3han5chou7@gmail.com>
2017-04-12 13:46:12 +0900

    don't double-closuring

--- src/hir/force_closure.rs
+++ src/hir/force_closure.rs
@@ -1,16 +1,25 @@
+use std::collections::HashSet;
+
 use hir::*;
 use pass::Pass;
 
-pub struct ForceClosure;
+
+pub struct ForceClosure {
+    functions: HashSet<Symbol>,
+}
 
 
 impl ForceClosure {
     pub fn new() -> Self {
-        ForceClosure
+        ForceClosure { functions: HashSet::new() }
     }
 
 
     fn conv_hir(&mut self, hir: &mut HIR) {
+        for val in hir.0.iter_mut() {
+            self.register_top_val(val)
+        }
+
         for val in hir.0.iter_mut() {
             self.conv_top_val(val)
         }
@@ -18,6 +27,14 @@ impl ForceClosure {
 
     fn conv_top_val(&mut self, val: &mut Val) {
         self.conv_expr(&mut val.expr, true);
+    }
+
+    fn register_top_val(&mut self, val: &mut Val) {
+        let mut bound_name = None;
+        if val.rec {
+            bound_name = Some(&val.name);
+        }
+        self.register_functions(&mut val.expr, bound_name);
     }
 
     fn conv_expr(&mut self, expr: &mut Expr, bound: bool) {
@@ -71,7 +88,7 @@ impl ForceClosure {
             }
 
             Sym { ref name, ref ty } => {
-                if !bound {
+                if !bound || !self.functions.contains(name) {
                     return;
                 }
                 match *ty {
@@ -90,6 +107,70 @@ impl ForceClosure {
 
         }
         *expr = assign;
+    }
+
+    fn register_functions(&mut self, expr: &mut Expr, bound_name: Option<&Symbol>) {
+        use hir::Expr::*;
+        match *expr {
+            Binds {
+                ref mut binds,
+                ref mut ret,
+                ..
+            } => {
+                for bind in binds.iter_mut() {
+                    let mut bound_name = None;
+                    if bind.rec {
+                        bound_name = Some(&bind.name);
+                    }
+                    self.register_functions(&mut bind.expr, bound_name);
+                }
+                self.register_functions(ret, None);
+                return;
+            }
+            Op {
+                ref mut l,
+                ref mut r,
+                ..
+            } => {
+                self.register_functions(l.as_mut(), None);
+                self.register_functions(r, None);
+                return;
+            }
+
+            Fun { ref mut body, .. } => {
+                match bound_name {
+                    Some(name) => {
+                        self.functions.insert(name.clone());
+                    }
+                    None => (),
+                };
+                self.register_functions(body, None);
+                return;
+            }
+            App {
+                ref mut fun,
+                ref mut arg,
+                ..
+            } => {
+                self.register_functions(fun, None);
+                self.register_functions(arg, None);
+                return;
+            }
+            If {
+                ref mut cond,
+                ref mut then,
+                ref mut else_,
+                ..
+            } => {
+                self.register_functions(cond, None);
+                self.register_functions(then, None);
+                self.register_functions(else_, None);
+                return;
+            }
+
+            Sym { .. } | Closure { .. } | Lit { .. } | PrimFun { .. } => return,
+
+        }
     }
 }
 
