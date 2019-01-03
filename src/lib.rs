a23946c7ef19689e3c06991dfb7b0c8439aad01a
κeen <3han5chou7@gmail.com>
2019-01-03 22:27:00 +0900

    cut out compile-pass test

--- src/lib.rs
+++ src/lib.rs
@@ -26,3 +26,34 @@ static BUILTIN_FUNCTIONS: &[&str] = &[
     "print", // "+", "-", "*", "div", "/", "mod",
              // "=", "<>", ">", ">=", "<", "<="
 ];
+
+pub fn compile_str<'a>(input: &'a str, config: &Config) -> Result<Vec<u8>, TypeError<'a>> {
+    use crate::pass::{ConvError, PrintablePass};
+    use wasm::Dump;
+
+    let id = id::Id::new();
+
+    let mut passes = compile_pass![
+       parse: ConvError::new(parse),
+       typing: ast::Typing::new(),
+       case_check: ast::CaseCheck::new(),
+       ast_to_hir: hir::AST2HIR,
+       rename: hir::Rename::new(id.clone()),
+       find_buildin: hir::FindBuiltin::new(),
+       flattening_expresion: hir::FlatExpr::new(id.clone()),
+       flattening_let: hir::FlatLet::new(),
+       unnest_functions: hir::UnnestFunc::new(id.clone()),
+       closure_conversion: hir::ForceClosure::new(),
+       hir_to_mir: mir::HIR2MIR::new(id.clone()),
+       unalias: mir::UnAlias::new(),
+       block_arrange: mir::BlockArrange::new(),
+       mir_to_lir: lir::MIR2LIR::new(),
+       backend: backend::LIR2WASM::new(),
+    ];
+
+    let module: wasm::Module = passes.trans(input, config)?;
+
+    let mut code = Vec::new();
+    module.dump(&mut code);
+    Ok(code)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,7 @@
 use clap::{app_from_crate, crate_authors, crate_description, crate_name, crate_version, Arg};
 use std::collections::HashSet;
 use std::fs;
-use wasm::Dump;
-use webml::pass::{ConvError, PrintablePass};
-use webml::*;
-
-fn compile_str<'a>(input: &'a str, config: &Config) -> Result<Vec<u8>, TypeError<'a>> {
-    let id = id::Id::new();
-
-    let mut passes = compile_pass![
-       parse: ConvError::new(parse),
-       typing: ast::Typing::new(),
-       case_check: ast::CaseCheck::new(),
-       ast_to_hir: hir::AST2HIR,
-       rename: hir::Rename::new(id.clone()),
-       find_buildin: hir::FindBuiltin::new(),
-       flattening_expresion: hir::FlatExpr::new(id.clone()),
-       flattening_let: hir::FlatLet::new(),
-       unnest_functions: hir::UnnestFunc::new(id.clone()),
-       closure_conversion: hir::ForceClosure::new(),
-       hir_to_mir: mir::HIR2MIR::new(id.clone()),
-       unalias: mir::UnAlias::new(),
-       block_arrange: mir::BlockArrange::new(),
-       mir_to_lir: lir::MIR2LIR::new(),
-       backend: backend::LIR2WASM::new(),
-    ];
-
-    let module: wasm::Module = passes.trans(input, config)?;
-
-    let mut code = Vec::new();
-    module.dump(&mut code);
-    Ok(code)
-}
+use webml::{compile_str, Config};
 
 fn main() {
     let matches = app_from_crate!()
@@ -68,25 +38,4 @@ fn main() {
     let input = fs::read_to_string(filename).expect("failed to load file");
     let code = compile_str(&input, &config).unwrap();
     fs::write("out.wasm", &code).unwrap()
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn examples_compile_pass() {
-        use walkdir::WalkDir;
-
-        let config = Config::default();
-        for entry in WalkDir::new("ml_example")
-            .into_iter()
-            .filter(|e| e.as_ref().map(|e| e.file_type().is_file()).unwrap_or(false))
-        {
-            let path = entry.unwrap().into_path();
-            let input = fs::read_to_string(&path).unwrap();
-            compile_str(&input, &config)
-                .expect(&format!("failed to compile {}", path.to_str().unwrap()));
-        }
-    }
 }
