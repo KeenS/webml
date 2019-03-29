4adfaff5c0bc0744dd083432b1eb25824ee021f2
κeen <3han5chou7@gmail.com>
2019-03-29 21:53:33 +0900

    cleanup code

--- webml-rt/src/lib.rs
+++ webml-rt/src/lib.rs
@@ -1,70 +1,65 @@
 #![no_std]
 #![cfg(target_arch = "wasm32")]
 use core::arch::wasm32::{memory_grow, memory_size};
+use core::mem;
 use core::panic::PanicInfo;
-use core::ptr;
 
 #[repr(C)]
 struct Page {
+    next: *mut Page,
+    size: usize,
     top: usize,
     data: *mut u8,
 }
 
 const MEMORY: u32 = 0;
+const WASM_PAGE_SIZE: usize = 64 * 1024;
+// GC page size including meta data
+const GC_PAGE_SIZE: usize = 1 * WASM_PAGE_SIZE;
 static mut GC: *mut Page = 0 as *mut _;
+static mut HEAD: *mut Page = 0 as *mut _;
 
-#[no_mangle]
-pub unsafe extern "C" fn init() {
+unsafe fn new_page() -> *mut Page {
     let ret = memory_grow(MEMORY, 1);
-    let page_ptr: *mut Page = ((ret as u32) * page_size()) as *mut _;
-    // for alignment
-    let data_ptr: *mut u8 = page_ptr.offset(8) as *mut _;
-    (*page_ptr).data = data_ptr;
-    GC = page_ptr as *mut _;
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn alloc(size: usize) -> *mut u8 {
-    let ret = (*GC).data.offset((*GC).top as isize);
-    (*GC).top += size;
-    ret
-}
-
-static mut HEAD: *mut *mut u8 = 0 as _;
-
-#[no_mangle]
-pub unsafe extern "C" fn page_alloc() -> *mut u8 {
-    if !HEAD.is_null() {
-        let next = *HEAD;
-        let ret = HEAD;
-        HEAD = next as *mut _;
-        return ret as *mut u8;
-    }
-
-    let ret = memory_grow(MEMORY, 1);
-
-    // if we failed to allocate a page then return null
+    // if we failed to allocate a page then panic
     if ret == usize::max_value() {
-        return ptr::null_mut();
+        // TODO: collect garbage
+        panic!("memory exhausted")
+    } else {
+        let page = (ret * WASM_PAGE_SIZE) as *mut u8 as *mut Page;
+        // next, top: relying wasm's page is 0 initialized
+        (*page).size = GC_PAGE_SIZE - mem::size_of::<Page>();
+        (*page).data = (page as *mut u8).offset(mem::size_of::<Page>() as isize);
+        page
     }
-
-    ((ret as u32) * page_size()) as *mut u8
 }
 
-#[no_mangle]
-pub unsafe extern "C" fn page_free(page: *mut u8) {
-    let page = page as *mut *mut u8;
-    *page = HEAD as *mut u8;
+unsafe fn add_new_page() {
+    let page = new_page();
+    (*HEAD).next = page;
     HEAD = page;
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn memory_used() -> usize {
-    (page_size() * (memory_size(MEMORY) as u32)) as usize
+pub unsafe extern "C" fn init() {
+    let page_ptr = new_page();
+    GC = page_ptr;
+    HEAD = GC;
 }
 
-fn page_size() -> u32 {
-    64 * 1024
+#[no_mangle]
+pub unsafe extern "C" fn alloc(size: usize) -> *mut u8 {
+    if (*HEAD).size <= (*HEAD).top + size {
+        add_new_page();
+    }
+    let ret = (*HEAD).data.offset((*HEAD).top as isize);
+    (*GC).top += size;
+    ret
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn memory_used() -> usize {
+    WASM_PAGE_SIZE * memory_size(MEMORY)
 }
 
 #[panic_handler]
