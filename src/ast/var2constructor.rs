121b13181b667159901ac793eb7347a9599bdb55
κeen <3han5chou7@gmail.com>
2019-11-18 03:04:38 +0900

    rewrite var2constructor with Transform

--- src/ast/var2constructor.rs
+++ src/ast/var2constructor.rs
@@ -1,4 +1,4 @@
-use crate::ast::util::Traverse;
+use crate::ast::util::Transform;
 use crate::ast::*;
 use crate::config::Config;
 use crate::pass::Pass;
@@ -23,93 +23,29 @@ impl VarToConstructor {
     fn symbol_table(&self) -> &SymbolTable {
         self.symbol_table.as_ref().unwrap()
     }
+
+    fn is_constructor(&self, name: &Symbol) -> bool {
+        self.symbol_table()
+            .get_datatype_of_constructor(&name)
+            .is_some()
+    }
 }
 
-impl<Ty: Clone + std::fmt::Debug> Traverse<Ty> for VarToConstructor {
-    fn traverse_expr(&mut self, expr: &mut Expr<Ty>) {
-        use crate::ast::Expr::*;
-        let new_expr = match expr {
-            Binds { ty, binds, ret } => {
-                self.traverse_binds(ty, binds, ret);
-                return;
-            }
-            BinOp { ty, op, l, r } => {
-                self.traverse_binop(ty, op, l, r);
-                return;
-            }
-            Fn { ty, param, body } => {
-                self.traverse_fn(ty, param, body);
-                return;
-            }
-            App { ty, fun, arg } => {
-                self.traverse_app(ty, fun, arg);
-                return;
-            }
-            If {
-                ty,
-                cond,
-                then,
-                else_,
-            } => {
-                self.traverse_if(ty, cond, then, else_);
-                return;
-            }
-            Case { ty, cond, clauses } => {
-                self.traverse_case(ty, cond, clauses);
-                return;
-            }
-            Tuple { ty, tuple } => {
-                self.traverse_tuple(ty, tuple);
-                return;
-            }
-            Constructor { ty, name } => {
-                self.traverse_constructor(ty, name);
-                return;
-            }
-            Literal { ty, value } => {
-                self.traverse_lit(ty, value);
-                return;
-            }
-            Symbol { ty, name }
-                if self
-                    .symbol_table()
-                    .get_datatype_of_constructor(name)
-                    .is_some() =>
-            {
-                Expr::Constructor {
-                    ty: ty.clone(),
-                    name: name.clone(),
-                }
-            }
-            Symbol { ty, name } => {
-                self.traverse_sym(ty, name);
-                return;
-            }
-        };
-
-        *expr = new_expr
+impl<Ty: std::fmt::Debug> Transform<Ty> for VarToConstructor {
+    fn transform_symbol(&mut self, ty: Ty, name: Symbol) -> Expr<Ty> {
+        if self.is_constructor(&name) {
+            Expr::Constructor { ty, name }
+        } else {
+            Expr::Symbol { ty, name }
+        }
     }
 
-    fn traverse_pattern(&mut self, pattern: &mut Pattern<Ty>) {
-        use crate::ast::Pattern::*;
-        let new_pattern = match pattern {
-            Constant { .. } | Constructor { .. } | Wildcard { .. } | Tuple { .. } => return,
-
-            Variable { ty, name }
-                if self
-                    .symbol_table()
-                    .get_datatype_of_constructor(name)
-                    .is_some() =>
-            {
-                Pattern::Constructor {
-                    ty: ty.clone(),
-                    name: name.clone(),
-                }
-            }
-            Variable { .. } => return,
-        };
-
-        *pattern = new_pattern;
+    fn transform_pat_variable(&mut self, ty: Ty, name: Symbol) -> Pattern<Ty> {
+        if self.is_constructor(&name) {
+            Pattern::Constructor { ty, name }
+        } else {
+            Pattern::Variable { ty, name }
+        }
     }
 }
 
@@ -118,11 +54,11 @@ impl<E, Ty: Clone + std::fmt::Debug> Pass<(SymbolTable, AST<Ty>), E> for VarToCo
 
     fn trans(
         &mut self,
-        (symbol_table, mut ast): (SymbolTable, AST<Ty>),
+        (symbol_table, ast): (SymbolTable, AST<Ty>),
         _: &Config,
     ) -> ::std::result::Result<Self::Target, E> {
         self.init(symbol_table);
-        self.traverse_ast(&mut ast);
+        let ast = self.transform_ast(ast);
         let symbol_table = self.generate_symbol_table();
         Ok((symbol_table, ast))
     }
