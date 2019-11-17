e6bc5f2a1d6df4eaa02adadfbe30c1d131df6d88
κeen <3han5chou7@gmail.com>
2019-11-18 03:58:24 +0900

    fix a typo

--- src/lib.rs
+++ src/lib.rs
@@ -35,7 +35,7 @@ pub fn compile_str<'a>(input: &'a str, config: &Config) -> Result<Vec<u8>, TypeE
        typing: ast::Typer::new(),
        case_check: ast::CaseCheck::new(),
        ast_to_hir: hir::AST2HIR::new(id.clone()),
-       find_buildin: hir::FindBuiltin::new(),
+       find_builtin: hir::FindBuiltin::new(),
        flattening_expression: hir::FlatExpr::new(id.clone()),
        flattening_let: hir::FlatLet::new(),
        unnest_functions: hir::UnnestFunc::new(id.clone()),
