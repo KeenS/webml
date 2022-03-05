1271ca0d88b1ac33cb6c62b6191e2dec45645c01
κeen <3han5chou7@gmail.com>
2022-03-05 14:56:14 +0900

    more pattern

--- tests/tests/parser.rs
+++ tests/tests/parser.rs
@@ -39,7 +39,7 @@ fn parse_char() {
 
 #[test]
 fn parse_string() {
-    let input = r##"val x = "abc\n""##;
+    let input = r##"val x = "abc\n\\""##;
     let ast = parse(input).unwrap();
     assert_eq!(
         ast,
@@ -54,7 +54,7 @@ fn parse_string() {
             expr: Expr {
                 ty: Empty {},
                 inner: ExprKind::D(DerivedExprKind::String {
-                    value: "abc\n".chars().map(|c| c as u32).collect()
+                    value: "abc\n\\".chars().map(|c| c as u32).collect()
                 })
             },
         },])
